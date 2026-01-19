4f96c7f7f3c554fdec5be37befd6da63b2584720
jullid <jullid@cosmos3.int.lunarc>
2026-01-19 09:49:14 +0100

    wrapped preprocessing.py into __main__ == '__main__' guard

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -9,17 +9,11 @@ import pandas as pd
 import warnings
 import sys
 
+
 DATA_DIR = Path.home() / "projects" / "data"
 OUTPUT_DIR = DATA_DIR / "processed"
 csv_file = DATA_DIR / "raw" /"GSE290585_SeSaMeBeta_MM285_BS.csv" # path to mouse csv file
 
-print(sys.executable)
-print(csv_file.exists())
-
-OUTPUT_DIR.mkdir(exist_ok=True)
-
-# Read CSV file:
-df = pd.read_csv(csv_file)
 
 def simplify_colname(name: str, n_underscores: int = 4) -> str:
     """
@@ -82,28 +76,42 @@ def reorder_columns_grouped(df):
     return df.iloc[:, new_idx_order]
 
 
-# we do not want to change the first column, so here we separate them
-first_col = df.columns[0]
-other_cols = df.columns[1:]
+def main():
+    print(sys.executable)
+    print(csv_file.exists())
 
-# loop through the columns
-simplified_other_cols = []
+    OUTPUT_DIR.mkdir(exist_ok=True)
 
-for col in other_cols:
-    new_col = simplify_colname(col, n_underscores=4)
-    simplified_other_cols.append(new_col)
+    # Read CSV file:
+    df = pd.read_csv(csv_file)
 
-# reconstruct the full column list with first column:
-new_columns = [first_col] + simplified_other_cols
+    # we do not want to change the first column, so here we separate them
+    first_col = df.columns[0]
+    other_cols = df.columns[1:]
 
-# assign new column names to the df
-df.columns = new_columns
+    # loop through the columns
+    simplified_other_cols = []
 
-# call re-order function:
-df_reordered = reorder_columns_grouped(df)
+    for col in other_cols:
+        new_col = simplify_colname(col, n_underscores=4)
+        simplified_other_cols.append(new_col)
 
-print(df_reordered.head())
+    # reconstruct the full column list with first column:
+    new_columns = [first_col] + simplified_other_cols
 
-output_path = OUTPUT_DIR / (csv_file.stem + "_simplified_columns.csv")
-df_reordered.to_csv(output_path, index=False)
-print(f"Saved to: {output_path}")
+    # assign new column names to the df
+    df.columns = new_columns
+
+    # call re-order function:
+    df_reordered = reorder_columns_grouped(df)
+
+    # visual validation
+    print(df_reordered.head())
+
+    output_path = OUTPUT_DIR / (csv_file.stem + "_simplified_columns.csv")
+    df_reordered.to_csv(output_path, index=False)
+    print(f"Saved to: {output_path}")
+
+
+if __name__ == "__main__":
+    main()
